7b5aa3e1507f36ee85d677d3209c739994f821ab
shyuuun <m4rshm4ll0w14@gmail.com>
2024-04-23 20:23:34 +0800

    :sparkles: layout for edit_profile.dart

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:i_explore/components/TextFieldComponent.dart';
+import 'package:i_explore/utils/colors.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
@@ -10,16 +12,132 @@ class EditProfile extends StatefulWidget {
 class _EditProfileState extends State<EditProfile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Test'),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [orangeOneColor, orangeTwoColor, orangeThreeColor],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Text('Edit Profile Page'),
-          )
-        ],
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: orangeOneColor,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              iconSize: 32,
+            ),
+          ),
+          title: Text(
+            'Edit Profile'.toUpperCase(),
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'FSP-Demo',
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          actions: [],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleAvatar(
+                      radius: 75,
+                    ),
+                    GestureDetector(
+                      onTap: () => print('Clicked edit photo'),
+                      child: Text('Change Photo',
+                          style: TextStyle(
+                            fontFamily: 'FSP-Demo',
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 20,
+                          )),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Personal Info:'.toUpperCase(),
+                      style: TextStyle(
+                        fontFamily: 'FSP-Demo',
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Form(
+                    child: Column(
+                  children: [
+                    TextFieldComponent(
+                      name: 'Full Name',
+                      icon: Icons.person,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextFieldComponent(
+                      name: 'Email',
+                      icon: Icons.mail,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextFieldComponent(
+                      name: 'Password',
+                      icon: Icons.lock,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () => print('Clicked Delete acc btn'),
+                            child: Text(
+                              'Delete your account?',
+                              style: TextStyle(
+                                  color: Colors.redAccent,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w800),
+                            )),
+                        ElevatedButton(
+                            onPressed: () => print('Clicked save acc btn'),
+                            child: Text(
+                              'Save',
+                              style: TextStyle(
+                                  color: Colors.redAccent,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w800),
+                            )),
+                      ],
+                    )
+                  ],
+                )),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:i_explore/components/HeaderComponent.dart';
 import 'package:i_explore/components/TextFieldComponent.dart';
-import 'package:i_explore/pages/homepage.dart';
 import 'package:i_explore/pages/register.dart';
 import 'package:i_explore/utils/colors.dart';
 import 'package:i_explore/utils/validator.dart';
@@ -19,39 +19,6 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  void _signInUser(String email, String password) async {
-    AuthService authService = Provider.of<AuthService>(context, listen: false);
-
-    try {
-      await authService.signInUser(email, password);
-      // Navigate to home page after successful authentication
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-            builder: (context) => const MyHomePage(title: "iExplore")),
-      );
-    } catch (e) {
-      // Handle authentication errors (e.g., show error dialog)
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Error'),
-            content: Text('Failed to login: ${e.toString()}'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(); // Close the dialog
-                },
-                child: const Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -72,9 +39,7 @@ class _LoginState extends State<Login> {
                 logoTitle: 'iExplore',
               ),
               const SizedBox(height: 25),
-              LoginForm(
-                onPressed: _signInUser, // Pass the correct callback function
-              ),
+              LoginForm(),
               const SizedBox(height: 50),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -114,20 +79,62 @@ class _LoginState extends State<Login> {
 }
 
 class LoginForm extends StatefulWidget {
-  final void Function(String, String) onPressed;
   const LoginForm({
     Key? key,
-    required this.onPressed,
   }) : super(key: key);
 
   @override
-  LoginFormState createState() => LoginFormState();
+  State<LoginForm> createState() => LoginFormState();
 }
 
 class LoginFormState extends State<LoginForm> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   final _loginFormKey = GlobalKey<FormState>();
+  bool _loading = false;
+
+  void _signInUser(BuildContext context) async {
+    if (_loginFormKey.currentState!.validate()) {
+      setState(() {
+        _loading = true; // Set loading state to true while signing in
+      });
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+      try {
+        AuthService authService =
+            Provider.of<AuthService>(context, listen: false);
+        final user = await authService.signInUser(email, password);
+        print('user: ${user}');
+        if (user != null) {
+          // Navigate to home page after successful authentication
+          context.go('/home');
+        }
+      } catch (e) {
+        // Handle authentication errors (e.g., show error dialog)
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Error'),
+              content: Text('Failed to login: ${e.toString()}'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } finally {
+        setState(() {
+          _loading = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -155,7 +162,7 @@ class LoginFormState extends State<LoginForm> {
             name: "Email",
             icon: Icons.email,
             validator: validateEmail,
-            textController: emailController,
+            textController: _emailController,
           ),
           const SizedBox(
             height: 25,
@@ -165,44 +172,34 @@ class LoginFormState extends State<LoginForm> {
             icon: Icons.password,
             obscureText: true,
             validator: validatePassword,
-            textController: passwordController,
+            textController: _passwordController,
           ),
           const SizedBox(
             height: 25,
           ),
-          GestureDetector(
-            onTap: () {
-              if (_loginFormKey.currentState!.validate()) {
-                widget.onPressed(
-                  emailController.text,
-                  passwordController.text,
-                );
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            const MyHomePage(title: "iExplore")));
-              }
-            },
-            child: Container(
-              padding: const EdgeInsets.all(25),
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: const Center(
-                child: Text(
-                  "Sign In",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+          _loading
+              ? CircularProgressIndicator()
+              : GestureDetector(
+                  onTap: () => _signInUser(context),
+                  child: Container(
+                    padding: const EdgeInsets.all(25),
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-          )
+                )
         ],
       ),
     );
